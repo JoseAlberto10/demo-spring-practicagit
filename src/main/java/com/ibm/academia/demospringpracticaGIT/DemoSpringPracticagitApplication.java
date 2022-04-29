77f566b38a934c2b7f5dce0fbda855033872f8b6
JoseAlberto10 <alberto_rmz10@outlook.com>
2022-04-29 14:22:40 -0500

    Agregue el proyecto de Spring prueba en git

---
+++ src/main/java/com/ibm/academia/demospringpracticaGIT/DemoSpringPracticagitApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.academia.demospringpracticaGIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringPracticagitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringPracticagitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/academia/demospringpracticaGIT/DemoSpringPracticagitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.academia.demospringpracticaGIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringPracticagitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
